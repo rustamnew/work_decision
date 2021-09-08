cfe42c9831ebaa52fdb3285a58cc44be03e85589
rustamnew <1rustamnew1@gmail.com>
2021-09-08 09:50:12 +0300

    fix

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,5 +1,13 @@
 <?php
 $arUrlRewrite=array (
+  1 => 
+  array (
+    'CONDITION' => '#^/services/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/services/index.php',
+    'SORT' => 100,
+  ),
   0 => 
   array (
     'CONDITION' => '#^/rest/#',
